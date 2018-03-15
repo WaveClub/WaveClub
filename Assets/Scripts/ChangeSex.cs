dfb804e236a260418cbf3b5db9154a007809c083
WaveClub <ooo.wave.club@gmail.com>
2018-03-16 01:07:35 +0300

    add registration

---
+++ Assets/Scripts/ChangeSex.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChangeSex : MonoBehaviour, IPointerDownHandler {
+	public GameObject currentSex;
+	public GameObject anotherSex;
+
+	public void OnPointerDown(PointerEventData eventData) {
+		currentSex.SetActive (true);
+		anotherSex.SetActive (false);
+	}
+}
