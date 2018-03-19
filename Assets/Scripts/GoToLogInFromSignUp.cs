6c740e8b2b991546b3af95c0d5e1ae58b1e129c6
WaveClub <Yauheni@Yauheni-PC MINGW64 /d/Unity/WaveClub (master)$ git initReinitialized existing Git repository in D:/Unity/WaveClub/.git/Yauheni@Yauheni-PC MINGW64 /d/Unity/WaveClub (master)$ git remote add origin https://github.com/WaveClub/WaveClub.gitfatal: remote origin already exists.Yauheni@Yauheni-PC MINGW64 /d/Unity/WaveClub (master)$ git commit -m comment*** Please tell me who you are.Run  git config --global user.email you@example.com  git config --global user.name Your>
2018-03-19 20:49:50 +0200

    Swipe 1.0

---
+++ Assets/Scripts/GoToLogInFromSignUp.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToLogInFromSignUp : MonoBehaviour {
+	public enum SwipeDirection{
+		Up,
+		Down,
+		Right,
+		Left
+	}
+
+	public static event System.Action<SwipeDirection> Swipe;
+	private bool swiping = false;
+	private bool eventSent = false;
+	private Vector2 lastPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount == 0) 
+			return;
+
+		if (Input.GetTouch(0).deltaPosition.sqrMagnitude != 0){
+			if (swiping == false){
+				swiping = true;
+				lastPosition = Input.GetTouch(0).position;
+				return;
+			}
+			else{
+				if (!eventSent) {
+					if (Swipe != null) {
+						Vector2 direction = Input.GetTouch(0).position - lastPosition;
+
+						if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)){
+							if (direction.x > 0) {
+								Debug.Log ("right");
+								//Swipe (SwipeDirection.Right);
+							} else {
+								Debug.Log ("left");
+								//Swipe (SwipeDirection.Left);
+							}
+						}
+						else{
+							if (direction.y > 0) {
+								Debug.Log ("up");
+								//Swipe (SwipeDirection.Up);
+							} else {
+								Debug.Log ("down");
+								//Swipe (SwipeDirection.Down);
+							}
+						}
+
+						eventSent = true;
+					}
+				}
+			}
+		}
+		else{
+			swiping = false;
+			eventSent = false;
+		}
+	}
+		
+	}
+
