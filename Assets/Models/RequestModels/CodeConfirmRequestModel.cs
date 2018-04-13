0721db211083f180a6e8b68b60e1907350a498bf
WaveClub <36933548+WaveClub@users.noreply.github.com>
2018-04-13 11:00:24 +0300

    add model

---
+++ Assets/Models/RequestModels/CodeConfirmRequestModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class CodeConfirmRequestModel {
+    public string userId { get; set; }
+    public string сode { get; set; }
+
+    public CodeConfirmRequestModel(string userId, string code) {
+        this.userId = userId;
+        this.сode = code;
+    }
+
+    public string SaveToString()
+    {
+        return JsonUtility.ToJson(this);
+    }
+}
